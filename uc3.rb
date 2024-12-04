e049fa054f4d90ec17b4c9be084e654e0cc7a80f
agrim106 <agrimchaudhary2@gmail.com>
2024-12-04 15:16:38 +0530

    Add partime Employee  & calculate Wage

---
+++ uc3.rb
@@ -0,0 +1,10 @@
+puts" Add Part-time Employee & wage "
+wage = 20 
+attendance = rand(2)
+
+if attendance == 1
+  partTime_wage = wage * 4
+  puts partTime_wage
+else
+  puts "No PartTime work Done."
+end
