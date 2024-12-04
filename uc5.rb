e0446dfe34b0767334afc6aaf95c2c044a3b41d6
agrim106 <agrimchaudhary2@gmail.com>
2024-12-04 15:20:24 +0530

    Calculating Monthly Wages. The Employee works 20 days a month.

---
+++ uc5.rb
@@ -0,0 +1,20 @@
+puts"Calculating Monthly Wages. The Employee works 20 days a month. " 
+puts"\n"
+part_time_hours = 4
+full_time_hours = 8
+per_hour_wage = 20
+monthly_wage = 0
+
+for i in 1..20
+  rng = Random.new
+  type_of_work = rng.rand(0..2)
+  if type_of_work == 0
+    monthly_wage += 0
+  elsif type_of_work == 1
+    monthly_wage += (part_time_hours*per_hour_wage)
+  elsif type_of_work == 2
+    monthly_wage += (full_time_hours*per_hour_wage)
+  end
+end
+
+puts "Monthly wage of employee : $ #{monthly_wage}"
